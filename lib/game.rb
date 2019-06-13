59fcd7fee181a8bea39060f3fb60ad5c347943cc
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2019-06-13 08:35:03 +0700

    [REFACTOR] extract list-word-stuff to class WordBank

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require 'tty-prompt'
 
 require 'player'
+require 'word_bank'
 
 class Game
 
@@ -12,16 +13,16 @@ class Game
 
     puts "Hello #{player.name}, let's play !"
 
-    list_word = ['book', 'bread', 'bike', 'car', 'house', 'table']
+    word_bank = WordBank.new
 
     while true
-      if list_word.empty?
+      if word_bank.empty?
         puts "Congratulation #{player.name}! You've solved all words with score: #{player.score}"
         break
       end
 
-      word = pick_word(list_word)
-      obs_word = obfuscate(word)
+      word = word_bank.pick_word()
+      obs_word = WordBank.obfuscate(word)
 
       answer = prompt.ask("Guess this word: #{obs_word} >>> ") do |q|
         q.validate(/\A#{word}\z/i, "WRONG. Try again. Guess this word: #{obs_word}")
@@ -29,30 +30,11 @@ class Game
       end
 
       player.inc_score
-      list_word = list_word - [word]
+      word_bank.remove_word(word)
 
       puts "RIGHT. Your score: #{player.score}"
     end
 
   end
 
-  def pick_word (list_word)
-    if !list_word.empty?
-      list_word[rand(list_word.length)]
-    else
-      ''
-    end
-  end
-
-  def obfuscate(word)
-    obs = shuffle(word)
-    while obs == word
-      obs = shuffle(word)
-    end
-    obs
-  end
-
-  def shuffle(word)
-    word.split("").shuffle.join
-  end
 end
---
+++ lib/word_bank.rb
@@ -0,0 +1,34 @@
+class WordBank
+  def initialize
+    @list_word = ['book', 'bread', 'bike', 'car', 'house', 'table']
+  end
+
+  def remove_word(word)
+    @list_word = @list_word - [word]
+  end
+
+  def empty?
+    @list_word.empty?
+  end
+
+  def pick_word
+    if !empty?
+      @list_word[rand(@list_word.length)]
+    else
+      nil
+    end
+  end
+
+  def self.obfuscate(word)
+    obs = self.shuffle(word)
+    while obs == word
+      obs = self.shuffle(word)
+    end
+    obs
+  end
+
+  def self.shuffle(word)
+    word.split("").shuffle.join
+  end
+
+end
